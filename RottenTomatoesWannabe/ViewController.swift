f919212f128dbb8f89c705da61509e47fd93a1fb
Bianca Curutan <bmcurutan@gmail.com>
2016-10-13 08:27:21 -0700

    no message

--- RottenTomatoesWannabe/ViewController.swift
+++ RottenTomatoesWannabe/ViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "") 
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
 }
 
